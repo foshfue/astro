9e783b0dba7e73abbca069089cd6b68d914b8b31
Kutay Sakallioglu <kutay@vestiyer.co>
2022-09-22 00:04:19 +0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
+  }
 });
